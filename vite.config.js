3f01ff722b73003bf8eeae6b77c655fb68d56007
banbeibingmeishi <3079693981@qq.com>
2023-07-25 14:31:21 +0800

    disposition

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite';
+import { VantResolver } from 'unplugin-vue-components/resolvers';
+import { fileURLToPath, URL } from 'node:url'
+import {resolve} from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      resolvers:[VantResolver()],
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build:{
+    rollupOptions:{
+      input:{
+        main: resolve(__dirname, 'index.html'),
+        person: resolve(__dirname, 'person/index.html')
+      }
+    }
+  }
 })
