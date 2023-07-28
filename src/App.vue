18c6c1327d5cf4d69f6b5803c4aa8563a0920323
JHY921 <452212248@qq.com>
2023-07-28 19:29:53 +0800

    文件整理

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,15 @@
 </template>
 
 <script setup>
-import Todo from './components/todo.vue'
-import ballStage from './components/ballStage.vue'
+import Todo from './components/homepage/todo.vue'
+import ballStage from './components/homepage/ballStage.vue'
 import { defineProps } from 'vue'
-import Proccess from './components/proccess.vue'
+import Proccess from './components/homepage/proccess.vue'
 // import heat1 from './components/heat/heat1.vue'
 // import heat2 from './components/heat/heat2.vue'
 // import heat3 from './components/heat/heat3.vue'
 // import heat from './components/heat/heat.vue'
 // Define props
-import forumArea from './components/forumArea.vue'
+import forumArea from './components/forum/forumArea.vue'
 const props = defineProps()
 </script>
