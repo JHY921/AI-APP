41758fcbb515d16eebc668ef6bf6204887897cdc
zheng ling jun <110295326+w1oss@users.noreply.github.com>
2023-07-30 19:07:41 +0800

    forumArea修改

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,7 @@
-<template>
-  <Todo />
-  <Proccess></Proccess>
-</template>
-
-<script setup>
+<script>
 import Todo from './components/homepage/todo.vue'
-import ballStage from './components/homepage/ballStage.vue'
-import { defineProps } from 'vue'
+// import ballStage from './components/homepage/ballStage.vue'
+// import { defineProps } from 'vue'
 import Proccess from './components/homepage/proccess.vue'
 // import heat1 from './components/heat/heat1.vue'
 // import heat2 from './components/heat/heat2.vue'
@@ -14,5 +9,11 @@ import Proccess from './components/homepage/proccess.vue'
 // import heat from './components/heat/heat.vue'
 // Define props
 import forumArea from './components/forum/forumArea.vue'
-const props = defineProps()
+// const props = defineProps()
 </script>
+
+<template>
+  <!-- <Todo /> -->
+  <!-- <Proccess></Proccess> -->
+  <forumArea/>
+</template>
