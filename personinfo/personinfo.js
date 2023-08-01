fbb76cdd4534f1ce4bfda3ab81930ce6eb9bce2f
JHY921 <452212248@qq.com>
2023-08-01 18:53:52 +0800

    文件修改

--- personinfo/personinfo.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import Personinfo from '../src/components/personifo/personinfo.vue'
-import '../src/assets/font/font.css'
-createApp(Personinfo).mount('#app')
-import { Tabbar, TabbarItem } from 'vant'
-import { Image as VanImage, Lazyload  } from 'vant'
-const app =createApp()
-app.use(VanImage).use(Lazyload).use(Tabbar).use(TabbarItem)
