5fdb37ef15eb45babbd8f2783e24e7881edfd8ee
JHY921 <452212248@qq.com>
2023-08-05 00:07:37 +0800

    添加vue-datepicker

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup>
-
+import writeTodo from './components/homepage/todo/writeTodo.vue';
 </script>
 <template>
   <div>
-    <router-view></router-view>
+    <!-- <router-view></router-view> -->
+    <writeTodo />
   </div>
 </template> 
---
+++ src/components/homepage/todo/writeTodo.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="total">
+    <form action="">
+      <input type="text" />
+      <div class="time">
+        <div class="begin">
+          <span>开始</span>
+          <time datetime=""></time>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+<style scoped>
+.total {
+  left: 394px;
+  top: 555px;
+  width: 328px;
+  height: 191px;
+  opacity: 1;
+  border-radius: 20px;
+  background: linear-gradient(
+    180deg,
+    rgba(26, 119, 128, 1) 0%,
+    rgba(255, 255, 255, 1) 18.82%,
+    rgba(255, 255, 255, 1) 32.64%,
+    rgba(20, 132, 156, 1) 93.21%,
+    rgba(255, 255, 255, 1) 100%
+  );
+}
+</style>
+<script setup>
+
+</script>
