781a848c9603166e445f5a5d28e5f359eded8ca9
JHY921 <452212248@qq.com>
2023-08-29 13:20:54 +0800

    第三页面路由

--- src/views/forum/forum_index.vue
+++ src/views/forum/forum_index.vue
@@ -8,7 +8,7 @@ import Forum from '../../components/forum/forumArea.vue'
       <van-tabbar-item replace to="/Home" icon="home-o">首页</van-tabbar-item>
       <van-tabbar-item replace to="/Forum" icon="search">论坛</van-tabbar-item>
       <van-tabbar-item replace to="/extraFunction" icon="friends-o" info="5"
-        >xx</van-tabbar-item
+        >体验馆</van-tabbar-item
       >
       <van-tabbar-item replace to="/Person" icon="setting-o" info="20"
         >我的</van-tabbar-item
--- src/views/homepage/homepage.vue
+++ src/views/homepage/homepage.vue
@@ -13,7 +13,7 @@ import Proccess from '../../components/homepage/proccess/proccess.vue'
     <van-tabbar-item replace to="/Home" icon="home-o">首页</van-tabbar-item>
     <van-tabbar-item replace to="/Forum" icon="search">论坛</van-tabbar-item>
     <van-tabbar-item replace to="/extraFunction" icon="friends-o" info="5"
-      >xx</van-tabbar-item
+      >体验馆</van-tabbar-item
     >
     <van-tabbar-item replace to="/Person" icon="setting-o" info="20"
       >我的</van-tabbar-item
