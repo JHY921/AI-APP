1aa2f8fe3b36cb68052baea258e79f6de6492b20
JHY921 <452212248@qq.com>
2023-08-30 12:06:37 +0800

    发布路由

--- src/components/forum/crisscross.vue
+++ src/components/forum/crisscross.vue
@@ -1,24 +1,35 @@
 <template>
-  <div class="crisscross">
-    <img src="../../assets/icons/forum/forumarea/发帖按钮.png">
+  <div class="crisscross" @click="topost">
+    <img src="../../assets/icons/forum/forumarea/发帖按钮.png" />
   </div>
 </template>
+<script>
+export default {
+  data () {
+
+  },
+  methods: {
+    topost () {
+      this.$router.push('./Forum_post')
+    }
+  }
+}
+</script>
 <style scoped>
-.crisscross img{
+.crisscross img {
   position: fixed;
-  bottom:90px;
-  left:300px;
+  bottom: 90px;
+  left: 300px;
   width: 45px;
   height: 45px;
   opacity: 1;
 }
-.crisscross p{
+.crisscross p {
   font-size: 60px;
-  color:#ffffff;
-  margin-top:-25%;
+  color: #ffffff;
+  margin-top: -25%;
   font-weight: 100;
-  margin-left:11%;
+  margin-left: 11%;
   transform: scale(1);
 }
-
 </style>
