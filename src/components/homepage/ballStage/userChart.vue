ab294c0264b6bef3cb8ddb755a5be78c258a4e02
Damingass <1808757178@qq.com>
2023-09-14 14:41:45 +0800

    用户图标更新

--- src/components/homepage/ballStage/userChart.vue
+++ src/components/homepage/ballStage/userChart.vue
@@ -3,30 +3,17 @@
     <div class="status_bar"></div>
 
     <div class="return_label">
-      <div
-        @click="backTolast"
-        style="display: inline-block; width: 40px; height: 100%"
-      >
-        <svg
-          transform="translate(13,16)"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-          width="9.70001220703125"
-          height="14.89013671875"
-          viewBox="0 0 9.70001220703125 14.89013671875"
-          fill="none"
-        >
-          <path
-            id="路径 1"
-            style="fill: #007994; opacity: 1"
-            d="M8.05786,14.6566l-7.7,-6.45001c-0.05033,-0.04217 -0.09607,-0.08884 -0.13721,-0.14001c-0.04114,-0.05117 -0.0769,-0.10587 -0.10727,-0.16408c-0.03037,-0.05822 -0.05477,-0.11884 -0.07319,-0.18186c-0.01843,-0.06303 -0.03053,-0.12724 -0.03631,-0.19264c-0.00578,-0.06541 -0.00512,-0.13075 0.00197,-0.19603c0.00709,-0.06527 0.02049,-0.12923 0.04018,-0.19187c0.01969,-0.06264 0.0453,-0.12276 0.07684,-0.18035c0.03153,-0.05759 0.06838,-0.11156 0.11055,-0.16189c0.03798,-0.04535 0.07966,-0.08699 0.12503,-0.12494l7.7,-6.44c0.05037,-0.04213 0.10436,-0.07893 0.16198,-0.11042c0.05762,-0.03149 0.11776,-0.05706 0.18041,-0.0767c0.06265,-0.01964 0.12662,-0.03299 0.1919,-0.04003c0.06528,-0.00704 0.13062,-0.00765 0.19603,-0.00182c0.0654,0.00583 0.1296,0.01798 0.19261,0.03645c0.06301,0.01848 0.12361,0.04292 0.1818,0.07333c0.05819,0.03041 0.11286,0.06621 0.164,0.10739c0.05115,0.04118 0.09778,0.08695 0.13991,0.13732c0.04212,0.05037 0.07893,0.10436 0.11042,0.16198c0.03149,0.05762 0.05705,0.11775 0.0767,0.18041c0.01964,0.06265 0.03298,0.12662 0.04003,0.1919c0.00704,0.06528 0.00765,0.13062 0.00182,0.19603c-0.00583,0.0654 -0.01798,0.1296 -0.03645,0.19261c-0.01848,0.06301 -0.04293,0.12361 -0.07334,0.1818c-0.03041,0.05819 -0.0662,0.11286 -0.10739,0.16401c-0.04118,0.05114 -0.08695,0.09777 -0.13732,0.1399l-7.69999,6.44l-0.64156,-0.76708l0.64214,-0.76659l7.7,6.44999c0.05033,0.0422 0.09607,0.08887 0.13721,0.14c0.04115,0.0512 0.0769,0.1059 0.10727,0.1641c0.03037,0.0582 0.05476,0.11883 0.07319,0.1819c0.01843,0.063 0.03053,0.1272 0.03631,0.1926c0.00577,0.0654 0.00511,0.13073 -0.00198,0.196c-0.00709,0.06527 -0.02048,0.12923 -0.04017,0.1919c-0.01969,0.06267 -0.04531,0.12277 -0.07684,0.1803c-0.03153,0.0576 -0.06838,0.11157 -0.11054,0.1619c-0.04217,0.05033 -0.08884,0.0961 -0.14001,0.1373c-0.05117,0.04113 -0.10587,0.07687 -0.16409,0.1072c-0.05821,0.0304 -0.11883,0.0548 -0.18185,0.0732c-0.06303,0.01847 -0.12724,0.03057 -0.19264,0.0363c-0.06541,0.0058 -0.13075,0.00513 -0.19603,-0.002c-0.06527,-0.00707 -0.12923,-0.02043 -0.19187,-0.0401c-0.06264,-0.01973 -0.12276,-0.04537 -0.18035,-0.0769c-0.05759,-0.03153 -0.11156,-0.06837 -0.16189,-0.1105z"
-          ></path>
+      <div @click="backTolast" style="display: inline-block; width: 40px; height: 100%">
+        <svg transform="translate(13,16)" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+          width="9.70001220703125" height="14.89013671875" viewBox="0 0 9.70001220703125 14.89013671875" fill="none">
+          <path id="路径 1" style="fill: #007994; opacity: 1"
+            d="M8.05786,14.6566l-7.7,-6.45001c-0.05033,-0.04217 -0.09607,-0.08884 -0.13721,-0.14001c-0.04114,-0.05117 -0.0769,-0.10587 -0.10727,-0.16408c-0.03037,-0.05822 -0.05477,-0.11884 -0.07319,-0.18186c-0.01843,-0.06303 -0.03053,-0.12724 -0.03631,-0.19264c-0.00578,-0.06541 -0.00512,-0.13075 0.00197,-0.19603c0.00709,-0.06527 0.02049,-0.12923 0.04018,-0.19187c0.01969,-0.06264 0.0453,-0.12276 0.07684,-0.18035c0.03153,-0.05759 0.06838,-0.11156 0.11055,-0.16189c0.03798,-0.04535 0.07966,-0.08699 0.12503,-0.12494l7.7,-6.44c0.05037,-0.04213 0.10436,-0.07893 0.16198,-0.11042c0.05762,-0.03149 0.11776,-0.05706 0.18041,-0.0767c0.06265,-0.01964 0.12662,-0.03299 0.1919,-0.04003c0.06528,-0.00704 0.13062,-0.00765 0.19603,-0.00182c0.0654,0.00583 0.1296,0.01798 0.19261,0.03645c0.06301,0.01848 0.12361,0.04292 0.1818,0.07333c0.05819,0.03041 0.11286,0.06621 0.164,0.10739c0.05115,0.04118 0.09778,0.08695 0.13991,0.13732c0.04212,0.05037 0.07893,0.10436 0.11042,0.16198c0.03149,0.05762 0.05705,0.11775 0.0767,0.18041c0.01964,0.06265 0.03298,0.12662 0.04003,0.1919c0.00704,0.06528 0.00765,0.13062 0.00182,0.19603c-0.00583,0.0654 -0.01798,0.1296 -0.03645,0.19261c-0.01848,0.06301 -0.04293,0.12361 -0.07334,0.1818c-0.03041,0.05819 -0.0662,0.11286 -0.10739,0.16401c-0.04118,0.05114 -0.08695,0.09777 -0.13732,0.1399l-7.69999,6.44l-0.64156,-0.76708l0.64214,-0.76659l7.7,6.44999c0.05033,0.0422 0.09607,0.08887 0.13721,0.14c0.04115,0.0512 0.0769,0.1059 0.10727,0.1641c0.03037,0.0582 0.05476,0.11883 0.07319,0.1819c0.01843,0.063 0.03053,0.1272 0.03631,0.1926c0.00577,0.0654 0.00511,0.13073 -0.00198,0.196c-0.00709,0.06527 -0.02048,0.12923 -0.04017,0.1919c-0.01969,0.06267 -0.04531,0.12277 -0.07684,0.1803c-0.03153,0.0576 -0.06838,0.11157 -0.11054,0.1619c-0.04217,0.05033 -0.08884,0.0961 -0.14001,0.1373c-0.05117,0.04113 -0.10587,0.07687 -0.16409,0.1072c-0.05821,0.0304 -0.11883,0.0548 -0.18185,0.0732c-0.06303,0.01847 -0.12724,0.03057 -0.19264,0.0363c-0.06541,0.0058 -0.13075,0.00513 -0.19603,-0.002c-0.06527,-0.00707 -0.12923,-0.02043 -0.19187,-0.0401c-0.06264,-0.01973 -0.12276,-0.04537 -0.18035,-0.0769c-0.05759,-0.03153 -0.11156,-0.06837 -0.16189,-0.1105z">
+          </path>
         </svg>
       </div>
       <div style="display: inline-block">
         <center>
-          <span
-            style="
+          <span style="
               font-size: 16px;
               font-weight: 700;
               letter-spacing: 0px;
@@ -36,8 +23,7 @@
               left: 0;
               right: 0;
               transform: translateY(-50%);
-            "
-          >
+            ">
             详细数据
           </span>
         </center>
@@ -45,19 +31,18 @@
     </div>
     <div class="chartsConatiner">
       <div class="chartsBlock" style="margin: 0">
-        <div
-          id="gaugeChart"
-          style="
+        <div id="gaugeChart" style="
             height: 200px;
             width: 200px;
             margin: 0px;
             display: inline-block;
-          "
-        ></div>
+          "></div>
         <div style="display: inline-block; width: 160px; height: 200px">
-          <div class="glassContainer">当前所处阶段</div>
-          <div
-            style="
+          <div class="glassContainer" style="font-weight: 600;">
+            当前所处阶段
+            <p style="float: right;">阶段{{ levelList[userLevel] }}&nbsp;&nbsp;&nbsp;</p>
+          </div>
+          <div style="
               width: 140px;
               height: 45px;
               border-top-left-radius: 5px;
@@ -70,12 +55,11 @@
               text-align: center;
               font-size: 20px;
               margin-left: 20px;
-            "
-          >
-            数据1
+              font-weight: 600;
+            ">
+            本周成长{{ getData.progress }}
           </div>
-          <div
-            style="
+          <div style="
               width: 140px;
               height: 45px;
               border-top-left-radius: 5px;
@@ -89,47 +73,35 @@
               font-size: 20px;
               margin-left: 20px;
               margin-top: 10px;
-            "
-          >
-            数据1
+              font-weight: 600;
+            ">
+            下一阶段{{ 1000 - getData.progress }}
           </div>
         </div>
       </div>
-      <div
-        class="chartsBlock"
-        id="stackedLine"
-        style="height: 186px; width: 360px"
-      ></div>
-      <div
-        class="chartsBlock"
-        style="
+      <div class="chartsBlock" id="stackedLine" style="height: 186px; width: 360px"></div>
+      <div class="chartsBlock" style="
           height: 186px;
           width: 360px;
           border-radius: 0px;
           border-top-left-radius: 10px;
           border-bottom-left-radius: 10px;
-        "
-      >
-        <div
-          id="pieChart"
-          style="
+          overflow: hidden;
+        ">
+        <div id="pieChart" style="
             display: inline-block;
             height: 186px;
             width: 186px;
             border-radius: 10px;
-          "
-        ></div>
-        <div
-          style="
+          "></div>
+        <div style="
             display: inline-block;
             height: 186px;
             width: 174px;
             border-radius: 10px;
             overflow: visible;
-          "
-        >
-          <div
-            style="
+          ">
+          <div style="
               height: 61px;
               width: 100%;
               border-top-left-radius: 10px;
@@ -139,11 +111,9 @@
               color: #007791;
               font-size: 18px;
               line-height: 61px;
-            "
-          >
+            ">
             &nbsp;&nbsp;&nbsp;&nbsp;数据分析:
-            <div
-              style="
+            <div style="
                 float: right;
                 border-top-left-radius: 16px;
                 border-bottom-left-radius: 16px;
@@ -154,14 +124,11 @@
                 line-height: 35px;
                 color: #ffffff;
                 text-shadow: 0 0 2px white;
-              "
-              @click="pieData1"
-            >
+              " @click="pieData1">
               &nbsp;&nbsp;&nbsp;结果
             </div>
           </div>
-          <div
-            style="
+          <div style="
               height: 61px;
               width: 100%;
               border-top-left-radius: 10px;
@@ -171,11 +138,9 @@
               color: #007791;
               font-size: 18px;
               line-height: 61px;
-            "
-          >
+            ">
             &nbsp;&nbsp;&nbsp;&nbsp;数据分析:
-            <div
-              style="
+            <div style="
                 float: right;
                 border-top-left-radius: 16px;
                 border-bottom-left-radius: 16px;
@@ -186,15 +151,12 @@
                 line-height: 35px;
                 color: #ffffff;
                 text-shadow: 0 0 2px white;
-              "
-              @click="pieData2"
-            >
+              " @click="pieData2">
               &nbsp;&nbsp;&nbsp;结果
             </div>
           </div>
 
-          <div
-            style="
+          <div style="
               height: 61px;
               width: 100%;
               border-top-left-radius: 10px;
@@ -204,11 +166,9 @@
               color: #007791;
               font-size: 18px;
               line-height: 61px;
-            "
-          >
+            ">
             &nbsp;&nbsp;&nbsp;&nbsp;数据分析:
-            <div
-              style="
+            <div style="
                 float: right;
                 border-top-left-radius: 16px;
                 border-bottom-left-radius: 16px;
@@ -219,19 +179,13 @@
                 line-height: 35px;
                 color: #ffffff;
                 text-shadow: 0 0 2px white;
-              "
-              @click="pieData3"
-            >
+              " @click="pieData3">
               &nbsp;&nbsp;&nbsp;结果
             </div>
           </div>
         </div>
       </div>
-      <div
-        class="chartsBlock"
-        id="barChart"
-        style="height: 210px; width: 360px"
-      ></div>
+      <div class="chartsBlock" id="barChart" style="height: 210px; width: 360px"></div>
     </div>
   </div>
 </template>
@@ -254,11 +208,9 @@
 
 .chartsBlock {
   position: relative;
-  background: linear-gradient(
-    180deg,
-    rgba(255, 255, 255, 1) 80%,
-    rgba(198, 229, 235, 0.4) 100%
-  );
+  background: linear-gradient(180deg,
+      rgba(255, 255, 255, 1) 80%,
+      rgba(198, 229, 235, 0.4) 100%);
   box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
   border-radius: 10px;
   margin-top: 10px;
@@ -307,7 +259,7 @@ import axios from 'axios'
 import api from '../../../api/api'
 export default {
   name: 'BarChart',
-  setup () {
+  setup() {
     const chartInstance = ref(null)
     onMounted(() => {
       chartInstance.value = echarts.init(document.getElementById('gaugeChart'))
@@ -321,7 +273,7 @@ export default {
         // width:800,
         // height:800,
         title: {
-          text: `距离xx还有`,
+          text: `距离下一阶段还有`,
           left: 'center',
           y: 10,
           textStyle: {
@@ -415,11 +367,24 @@ export default {
       var xAxisData = []
       var data1 = []
       var data2 = []
-      for (var i = 0; i < 100; i++) {
-        xAxisData.push('数据' + i)
-        data1.push(i)
-        data2.push(2 * i)
+      var day = ['一', '二', '三', '四', '五', '六', '日']
+      for (var i = 0; i < 70; i++) {
+        //xAxisData.push('星期' + i)
+        xAxisData.push(0)
+        if (i % 10 == 0) xAxisData[i] = ('星期' + day[i / 10])
+        data1.push(0)
+        data2.push(0)
       }//数据
+      data1[0] = 4; data1[10] = 6; data1[20] = 9; data1[30] = 9; data1[40] = 17; data1[50] = 21; data1[60] = 30; data1[70] = 32;
+      for (var i = 0; i < 7; i++)
+        for (var j = i * 10; j < (i + 1) * 10; j++) {
+          if (j % 10 != 0) data1[j] = data1[j - 1] + (data1[(i + 1) * 10] - data1[i * 10]) / 9;
+        }
+        data2[0] = 6; data2[10] = 9; data2[20] = 11; data2[30] = 19; data2[40] = 26; data2[50] = 30; data2[60] = 31; data2[70] = 36;
+      for (var i = 0; i < 7; i++)
+        for (var j = i * 10; j < (i + 1) * 10; j++) {
+          if (j % 10 != 0) data2[j] = data2[j - 1] + (data2[(i + 1) * 10] - data2[i * 10]) / 9;
+        }
       option = {
         grid: { // 让图表占满容器
           top: "50px",
@@ -429,7 +394,7 @@ export default {
         },
         title: [
           {
-            text: '数据介绍',
+            text: '本周TODOList',
             x: 20,
             y: 10,
             textStyle: {
@@ -437,8 +402,8 @@ export default {
             },
           },
           {
-            subtext: '注释：这里是注释内容',
-            x: 105,
+            subtext: '总数/完成数',
+            x: 146,
             y: 5,
             subtextStyle: {
               color: "#707F82",
@@ -462,12 +427,18 @@ export default {
           // }下载，导出图片等功能
         },
         tooltip: {},//提示框
-        xAxis: {
-          data: xAxisData,
-          splitLine: {
-            show: true,
+        xAxis: [
+          {
+            data: xAxisData,
+            splitLine: {
+              show: true,
+            },
+            axisLabel: {
+              interval: 9
+            }
           }
-        },
+
+        ],
         yAxis: {
           splitLine: {
             show: true,
@@ -537,6 +508,8 @@ export default {
               show: true, // 隐藏数据和指示线
               normal: {
                 position: 'inner',
+                color: '#FFFFFF',
+                fontWeight: 1000
               }
             },
             // labelLine: {
@@ -549,11 +522,11 @@ export default {
               borderRadius: 10
             },
             data: [
-              { value: 40, name: 'rose 1' },
-              { value: 38, name: 'rose 2' },
-              { value: 32, name: 'rose 3' },
-              { value: 40, name: 'rose 4' },
-              { value: 50, name: 'rose5' },
+              { value: 10, name: '收藏' },
+              { value: 38, name: '点赞' },
+              { value: 32, name: '浏览' },
+              { value: 20, name: '关注' },
+              { value: 50, name: '学习' },
             ]
           }
         ]
@@ -575,7 +548,7 @@ export default {
           bottom: "50px"
         },
         title: {
-          text: "数据介绍",
+          text: "综合评价",
           x: 140,
           y: 10,
           textStyle: {
@@ -596,14 +569,14 @@ export default {
           // }
         },
         legend: {
-          data: ['数据1', '数据2', '数据3'],
+          data: ['你的成绩', '预计成绩', '往期成绩'],
           y: 180,
           x: 10,
         },
         series: [
 
           {
-            name: '数据1',
+            name: '你的成绩',
             itemStyle: {
               normal: {
                 barBorderRadius: [20, 20, 0, 0]//柱形图圆角设置
@@ -615,7 +588,7 @@ export default {
             type: 'bar'
           },
           {
-            name: '数据2',
+            name: '预计成绩',
             itemStyle: {
               normal: {
                 barBorderRadius: [20, 20, 0, 0]//柱形图圆角设置
@@ -625,7 +598,7 @@ export default {
             type: 'bar',
           },
           {
-            name: '数据3',
+            name: '往期成绩',
             itemStyle: {
               normal: {
                 barBorderRadius: [20, 20, 0, 0]//柱形图圆角设置
@@ -643,37 +616,41 @@ export default {
       chartInstance
     }
   },
-  data () {
+  data() {
     return {
+      userLevel: 2,
+      levelList: ['一', '二', '三', '四', '五', '六', '七'],
+      getData: [],
     }
   },
-  created(){
+  created() {
     axios.get(`http://${api.api}/userChat`, {
-      headers:{'Authorization': 'Bearer '+localStorage.getItem('token')},
-      withCredentials:true
-    }).then(res=>{
+      headers: { 'Authorization': 'Bearer ' + localStorage.getItem('token') },
+      withCredentials: true
+    }).then(res => {
       console.log(res.data);
-    }).catch(err=>{
+      this.getData = res.data;
+    }).catch(err => {
       console.log(err);
     })
   },
   methods: {
-    click_follow () {
+    click_follow() {
       alert("follow")
     },
-    backTolast () {
+    backTolast() {
       history.back()
     },
-    moreInfor () {
+    moreInfor() {
       alert("more")
     },
-    pieData1 () {
+    pieData1() {
 
     },
-    pieData2 () {
+    pieData2() {
 
     },
-    pieData3 () {
+    pieData3() {
 
     },
   }
